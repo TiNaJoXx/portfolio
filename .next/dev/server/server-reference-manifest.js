c7e4af916a9d057f1f5c0fdf3bb06b013f642c81
Javier Tinajo Gallardo <javiertinajogallardo@MacBook-Pro-de-Javier-2.local>
2025-12-22 18:34:07 +0100

    refactor

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NqryO3JTLTClRERsyXe4gYv1EDI+kRUNntHBwSCbX0g=\"\n}"
---
+++ .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__6e902a3e._.css"
+],
+    source: "dynamic"
+});
